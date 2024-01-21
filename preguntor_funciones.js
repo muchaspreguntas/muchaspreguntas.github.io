acde3d29c67265e5e0691fdc966f8c8daa5c38b5
kristian Martinez <97622740+preguntor@users.noreply.github.com>
2024-01-21 11:40:45 -0500

    vibracion 2

--- preguntor_funciones.js
+++ preguntor_funciones.js
@@ -361,8 +361,7 @@ const questions = {
 };
 
 
-// Ruta del archivo de audio
-const audioUrl = "audio_boton_siguiente.mp3"; // Reemplaza con la ruta correcta de tu archivo de audio
+// ...
 
 // Función para mostrar la siguiente pregunta y vibrar el dispositivo
 function nextQuestion() {
@@ -395,25 +394,18 @@ function nextQuestion() {
   vibrateDevice();
 }
 
-// Función para reproducir el audio
-function playAudio() {
-  const audio = new Audio(audioUrl);
-  audio.play();
-}
+// ...
+
+// Evento de clic en el botón de siguiente pregunta
+document.getElementById('nextButton').addEventListener('click', nextQuestion);
 
 // Función para vibrar el dispositivo
 function vibrateDevice() {
   // Verifica si el navegador admite la API de vibración y si el usuario ha concedido permisos
-  if ('vibrate' in navigator) {
+  if ('vibrate' in navigator && window.location.protocol === 'https:') {
     navigator.vibrate([200]); // Vibra durante 200 milisegundos
   }
 }
 
-// Función para capitalizar la primera letra de una cadena
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+// ...
 
-document.querySelector('.menu-icon').addEventListener('click', function() {
-  document.querySelector('.nav-links').classList.toggle('show');
-});
