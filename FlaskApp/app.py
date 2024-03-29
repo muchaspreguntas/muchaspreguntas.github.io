6d2e5e3e76ef665a853564e4cd8f7898435be03f
kristian Martinez <97622740+preguntor@users.noreply.github.com>
2024-03-29 21:45:50 +0000

    prueba

---
+++ FlaskApp/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+from flask import Flask
+app = Flask (__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+if __name__ =="__main__":
+    app.run()
+
+
+##http://localhost:5000/ h
